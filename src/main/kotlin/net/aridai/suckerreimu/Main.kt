068ac398a8322f30771a79a80fc7b49bdcb33928
aridai <aridai.net@outlook.jp>
2023-01-05 21:03:00 +0900

    Loggerを作成

--- src/main/kotlin/net/aridai/suckerreimu/Main.kt
+++ src/main/kotlin/net/aridai/suckerreimu/Main.kt
@@ -1,5 +1,13 @@
 package net.aridai.suckerreimu
 
+import kotlinx.browser.document
+
 private fun main() {
-    console.log("Hello World!")
+    //  ログの出力設定を行う。
+    Logger.isEnabled = !isReleaseBuild
+    Logger.i { "スクリプト処理開始" }
 }
+
+//  リリースビルドで実行されているかどうかを判定する。
+//  (NOTE: GitHub Pagesでホストするため、リリース環境では常にHTTPS化がされているものとする。)
+private val isReleaseBuild: Boolean get() = document.location?.protocol?.lowercase()?.contains("https") ?: false
